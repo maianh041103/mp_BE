d267c173be65fd1c8d3d041052bf46be12657571
jirachi <daigiachantran01@gmail.com>
2024-04-06 09:25:36 +0700

    update api docs

--- src/mpModules/product/constant.js
+++ src/mpModules/product/constant.js
@@ -59,7 +59,6 @@ export const productIncludes = [
             "exchangeValue",
             "price",
             "productId",
-            "quantity",
             "code",
             "barCode",
             "isDirectSale",
