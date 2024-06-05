6659c7f7199a0f1bd92d97f76d4f39993f4963d3
maianh041103 <nguyenmaianh041103@gmail.com>
2024-06-05 14:16:05 +0700

    product master them filter productUnitId

--- src/mpModules/product/productMasterService.js
+++ src/mpModules/product/productMasterService.js
@@ -1,6 +1,6 @@
-import {findAllBatchByListProduct, findAllBatchByProductId} from "../batch/batchService";
-import {getInventory} from "../inventory/inventoryService";
-import {productStatuses} from "./productConstant";
+import { findAllBatchByListProduct, findAllBatchByProductId } from "../batch/batchService";
+import { getInventory } from "../inventory/inventoryService";
+import { productStatuses } from "./productConstant";
 
 const { PAGE_LIMIT } = require("../../helpers/choices");
 const Sequelize = require("sequelize");
@@ -51,10 +51,13 @@ export const productMasterIncludes = [
 export async function indexMasterSaleProducts(params) {
   const limit = +params.limit || 10;
   const page = +params.page || 1;
-  const { storeId, branchId, keyword } = params;
+  const { storeId, branchId, keyword, productUnit } = params;
   const where = {};
   if (storeId) {
     where.storeId = storeId;
+  }
+  if (productUnit) {
+    where.id = productUnit;
   }
   if (keyword) {
     const _products = await models.Product.findAll({
@@ -81,7 +84,8 @@ export async function indexMasterSaleProducts(params) {
       productId: {
         [Op.in]: productIds
       }
-    } }
+    }
+  }
   const [items, count] = await Promise.all([
     models.ProductUnit.findAll({
       attributes: [
@@ -156,7 +160,7 @@ export async function indexMasterSaleProducts(params) {
     const inventory = await getInventory(branchId, item.productId)
     item.dataValues.product.dataValues.quantity = inventory
     item.dataValues.quantity = parseInt(
-        inventory / item.exchangeValue
+      inventory / item.exchangeValue
     );
 
     if (!params.isSale) {
@@ -209,43 +213,44 @@ export async function indexMasterInboundProducts(params) {
       productId: {
         [Op.in]: productIds
       }
-  } }
-  const {rows, count} = await models.ProductUnit.findAndCountAll({
-      attributes: ["id",
-        "unitName",
-        "exchangeValue",
-        "price",
-        "productId",
-        "code",
-        "barCode",
-        "isDirectSale",
-        "isBaseUnit",
-        "point",
-        "storeId",
-        "branchId",
-          "createdAt"
-      ],
-      include: [
-        {
-          model: models.Product,
-          as: "product",
-          attributes: productAttributes,
-          include: productIncludes.filter(
-              (productInclude) => !ignoreAliasModels.includes(productInclude.as)
-          ),
-        },
-        ...productMasterIncludes,
-      ],
-      where: where,
-      offset: +limit * (+page - 1),
-      limit: +limit,
+    }
+  }
+  const { rows, count } = await models.ProductUnit.findAndCountAll({
+    attributes: ["id",
+      "unitName",
+      "exchangeValue",
+      "price",
+      "productId",
+      "code",
+      "barCode",
+      "isDirectSale",
+      "isBaseUnit",
+      "point",
+      "storeId",
+      "branchId",
+      "createdAt"
+    ],
+    include: [
+      {
+        model: models.Product,
+        as: "product",
+        attributes: productAttributes,
+        include: productIncludes.filter(
+          (productInclude) => !ignoreAliasModels.includes(productInclude.as)
+        ),
+      },
+      ...productMasterIncludes,
+    ],
+    where: where,
+    offset: +limit * (+page - 1),
+    limit: +limit,
     order: [["createdAt", "DESC"]]
-    });
+  });
   for (const item of rows) {
     const inventory = await getInventory(branchId, item.productId)
     item.dataValues.product.dataValues.quantity = inventory
     item.dataValues.quantity = parseInt(
-        inventory / item.exchangeValue
+      inventory / item.exchangeValue
     );
   }
 
@@ -258,7 +263,7 @@ export async function indexMasterInboundProducts(params) {
 }
 
 export async function detailMaster(query) {
-  return  await models.ProductMaster.findOne({
+  return await models.ProductMaster.findOne({
     attributes: [
       "storeId",
       "branchId",
