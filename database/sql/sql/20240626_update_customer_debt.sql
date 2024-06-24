76197670706432ff7d829560a821f7a63a08dae2
maianh041103 <nguyenmaianh041103@gmail.com>
2024-06-24 11:49:30 +0700

    Fix history point

--- database/sql/sql/20240626_update_customer_debt.sql
+++ database/sql/sql/20240626_update_customer_debt.sql
@@ -3,3 +3,15 @@ ALTER TABLE customer_debts
 DROP FOREIGN KEY customer_debts_ibfk_1;
 ALTER TABLE customer_debts
 DROP COLUMN transactionId;
+
+
+-- update table point_history
+ALTER TABLE point_history
+ADD COLUMN saleReturnId int unsigned null;
+
+ALTER TABLE point_history
+ADD CONSTRAINT point_history_ibfk_3
+FOREIGN KEY (saleReturnId) REFERENCES sale_returns(id);
+
+ALTER TABLE point_history
+ADD COLUMN code nvarchar(255) null;
