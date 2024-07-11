7348ec9dcb7bd485099d8cffdd04399d5f352960
maianh041103 <nguyenmaianh041103@gmail.com>
2024-07-11 17:46:44 +0700

    validator saleReturn

--- src/mpModules/saleReturn/saleReturnValidator.js
+++ src/mpModules/saleReturn/saleReturnValidator.js
@@ -12,7 +12,7 @@ const createSchema = Joi.object().keys({
     discount: Joi.number().integer().allow(null),
     returnFee: Joi.number().integer().allow(null),
     paid: Joi.number().integer().allow(null),
-    description: Joi.string().allow(null),
+    description: Joi.string().allow(null).allow(""),
     paymentType: Joi.string().valid("CASH", "BANK", "DEBT").allow(null),
     products: Joi.array()
         .items(
