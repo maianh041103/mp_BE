fbd46f970c6a4f835dcf02ffe420ac8f6cb09ec2
maianh041103 <nguyenmaianh041103@gmail.com>
2024-07-15 15:24:39 +0700

    Fix bug ...

--- database/models/mp_models/WarehouseCard.js
+++ database/models/mp_models/WarehouseCard.js
@@ -1,64 +1,69 @@
 'use strict';
 module.exports = (sequelize, Sequelize) => {
   const WarehouseCard = sequelize.define('WarehouseCard', {
-        id: {
-          type: Sequelize.INTEGER(11).UNSIGNED,
-          primaryKey: true,
-          autoIncrement: true,
-          allowNull: false,
-        },
-        code: {
-          allowNull: true,
-          type: Sequelize.STRING
-        },
-          productId: {
-              allowNull: false,
-              type: Sequelize.INTEGER(11).UNSIGNED,
-          },
-        productUnitId: {
-          allowNull: true,
-          type: Sequelize.INTEGER(11).UNSIGNED,
-        },
-        branchId: {
-          allowNull: true,
-          type: Sequelize.INTEGER(11).UNSIGNED,
-        },
-        partner: {
-          allowNull: true,
-          type: Sequelize.STRING
-        },
-      changeQty: {
-          allowNull: true,
-          type: Sequelize.INTEGER,
-      },
-      remainQty: {
-          allowNull: true,
-          type: Sequelize.INTEGER,
-      },
-        // 1. Ban hang , 2. Nhap hang,  3. Kiem kho
-        type: {
-          allowNull: false,
-          type: Sequelize.INTEGER(1).UNSIGNED,
-        },
-        createdAt: {
-          allowNull: true,
-          type: Sequelize.DATE
-        },
-        updatedAt: {
-          allowNull: true,
-          type: Sequelize.DATE
-      },
-      },{
-          tableName: 'warehouse_card',
-          timestamps: false
-      }
+    id: {
+      type: Sequelize.INTEGER(11).UNSIGNED,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    code: {
+      allowNull: true,
+      type: Sequelize.STRING
+    },
+    productId: {
+      allowNull: false,
+      type: Sequelize.INTEGER(11).UNSIGNED,
+    },
+    productUnitId: {
+      allowNull: true,
+      type: Sequelize.INTEGER(11).UNSIGNED,
+    },
+    branchId: {
+      allowNull: true,
+      type: Sequelize.INTEGER(11).UNSIGNED,
+    },
+    partner: {
+      allowNull: true,
+      type: Sequelize.STRING
+    },
+    changeQty: {
+      allowNull: true,
+      type: Sequelize.INTEGER,
+    },
+    remainQty: {
+      allowNull: true,
+      type: Sequelize.INTEGER,
+    },
+    // 1. Ban hang , 2. Nhap hang,  3. Kiem kho
+    type: {
+      allowNull: false,
+      type: Sequelize.INTEGER(1).UNSIGNED,
+    },
+    createdAt: {
+      allowNull: true,
+      type: Sequelize.DATE
+    },
+    updatedAt: {
+      allowNull: true,
+      type: Sequelize.DATE
+    },
+  }, {
+    tableName: 'warehouse_card',
+    timestamps: false
+  }
   );
-    WarehouseCard.associate = function (models) {
-        WarehouseCard.belongsTo(models.Branch, {
-            as: "branch",
-            foreignKey: "branchId",
-            targetKey: "id",
-        });
-    }
-    return WarehouseCard
+  WarehouseCard.associate = function (models) {
+    WarehouseCard.belongsTo(models.Branch, {
+      as: "branch",
+      foreignKey: "branchId",
+      targetKey: "id",
+    });
+    WarehouseCard.belongsTo(models.ProductUnit, {
+      as: "productUnit",
+      foreignKey: "productUnitId",
+      targetKey: "id",
+    });
+  }
+  return WarehouseCard
 };
