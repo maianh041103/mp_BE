02384a5884e0fa623a0ed30f032719a4f45b7a6a
maianh041103 <nguyenmaianh041103@gmail.com>
2024-08-24 11:21:13 +0700

    fix MEP-962

---
+++ src/mpModules/marketConfig/groupAgencyValidator.js
@@ -0,0 +1,30 @@
+import { respondWithError } from "../../helpers/response";
+
+const BaseJoi = require("@hapi/joi");
+const Extension = require("@hapi/joi-date");
+
+const Joi = BaseJoi.extend(Extension);
+
+const { ERROR_CODE_INVALID_PARAMETER } = require("../../helpers/errorCodes");
+
+const createSchema = Joi.object().keys({
+    name: Joi.string().trim().required(),
+    description: Joi.string().allow(null).allow(""),
+    branchId: Joi.number().integer().min(0).required()
+});
+
+export function createValidator(req, res, next) {
+    const { body } = req;
+    const result = Joi.validate(body, createSchema);
+    if (result.error) {
+        res.json(
+            respondWithError(
+                ERROR_CODE_INVALID_PARAMETER,
+                result.error.message,
+                result.error.details
+            )
+        );
+        return;
+    }
+    next();
+}
